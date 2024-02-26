298fcbc8871082df48295f50b0669bcf835f3c8c
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-26 19:40:00 +0530

    [ADDED] : YOGESH Employee Information Project using Data Abstraction Industry level

---
+++ EmployeeInfoDataAbstraction/src/com/yogesh/employeeInfo/EmployeeMain.java
@@ -0,0 +1,46 @@
+package com.yogesh.employeeInfo;
+ 
+import java.util.Scanner;
+
+import com.yogesh.employeeinfomodel.EmployeeInfoModel;
+
+
+public class EmployeeMain {
+
+	EmployeeInfoModel employeeModel = new EmployeeInfoModel();
+	
+	public void setEmployeeData()
+	{
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Enter Employee Id :");
+		employeeModel.setEmployeeId(scanner.nextInt());
+		
+		System.out.print("Enter Employee Name :");
+		employeeModel.setEmployeeName(scanner.next());
+		
+		System.out.print("Enter Employee Mobile No :");
+		employeeModel.setEmployeeMobileNo(scanner.next());
+		
+		System.out.print("Enter Employee Salary :");
+		employeeModel.setEmployeeSalary(scanner.nextDouble());
+	}
+	
+	public void getEmployeeData()
+	{
+		 System.out.println("Employee ID :" + employeeModel.getEmployeeId());
+		 System.out.println("Employee Name :" + employeeModel.getEmployeeName());
+		 System.out.println("Employee Mobile No :" + employeeModel.getEmployeeMobileNo());
+		 System.out.println("Employee Salary :" + employeeModel.getEmployeeSalary()); 
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	EmployeeMain employeemain = new EmployeeMain();
+	employeemain.setEmployeeData();
+	employeemain.getEmployeeData();
+
+	}
+
+}
